34ffd33696a954a20af2329b1ba2929baf9370a4
Måns Bernhardt <mans.bernhardt@gmail.com>
2022-07-03 13:09:38 +0200

    Updated ObservedModel to work on ModelContainer instead of only Model to allow e.g. optional models. Extended ModelContainer with models property.

--- Sources/OneState/SwiftUI/ObservedModel.swift
+++ Sources/OneState/SwiftUI/ObservedModel.swift
@@ -2,17 +2,16 @@
 import SwiftUI
 
 @propertyWrapper
-@dynamicMemberLookup
-public struct ObservedModel<M: Model>: DynamicProperty {
-    @StateObject private var access = ModelAccess<M.State>()
+public struct ObservedModel<M: ModelContainer>: DynamicProperty {
+    @StateObject private var access = ModelAccess()
 
     public init(wrappedValue: M) {
         self.wrappedValue = wrappedValue
-        checkedContext()
+        wrappedValue.checkedContexts()
     }
 
     public var wrappedValue: M {
-        didSet { checkedContext() }
+        didSet { wrappedValue.checkedContexts() }
     }
 
     public var projectedValue: Self {
@@ -20,40 +19,31 @@ public struct ObservedModel<M: Model>: DynamicProperty {
     }
 
     public mutating func update() {
-        let context = checkedContext()
-        let prevContext = access.context
-        access.context = context
-        if wrappedValue.modelState?.storeAccess !== access {
+        let contexts = wrappedValue.checkedContexts()
+        let prevContexts = access.contexts
+
+        wrappedValue = M.modelContainer(from: wrappedValue.models.compactMap { model in
             StoreAccess.$current.withValue(Weak(value: access)) {
-                wrappedValue = M(context: context)
+                M.ModelElement(context: model.context)
             }
-        }
+        })
 
-        guard context !== prevContext else { return }
+        guard !contexts.elementsEqual(prevContexts, by: ===) else { return }
 
-        prevContext?.releaseFromView()
-        wrappedValue.retain()
-        access.startObserve()
+        prevContexts.forEach { $0.releaseFromView() }
+        wrappedValue.models.forEach { $0.retain() }
+        access.startObserving(from: contexts)
+        access.objectWillChange.send()
     }
 }
 
-extension ObservedModel: StoreViewProvider {
+extension ObservedModel: StoreViewProvider where M: Model {
     public var storeView: StoreView<M.State, M.State, Write> {
         wrappedValue.storeView
     }
 }
 
-private extension ObservedModel {
-    @discardableResult
-    func checkedContext() -> Context<M.State> {
-        guard let context = wrappedValue.modelState?.context as? Context<M.State> else {
-            fatalError("Model \(type(of: wrappedValue)) must be created via a @StateModel or the provide initializer taking a ViewStore. This is required for the view models state to be hooked up to view into a store.")
-        }
-        return context
-    }
-}
-
-public struct UsingModel<M: Model, Content: View>: View {
+public struct UsingModel<M: ModelContainer, Content: View>: View {
     @ObservedModel var model: M
     var content: (M) -> Content
 
@@ -64,6 +54,18 @@ public struct UsingModel<M: Model, Content: View>: View {
 
     public var body: some View {
         content(model)
+    }
+}
+
+private extension ModelContainer {
+    @discardableResult
+    func checkedContexts() -> [Context<ModelElement.State>] {
+        models.map { model in
+            guard let context = model.modelState?.context as? Context<ModelElement.State> else {
+                fatalError("Model \(type(of: self)) must be created via a @StateModel or the provide initializer taking a ViewStore. This is required for the view models state to be hooked up to view into a store.")
+            }
+            return context
+        }
     }
 }
 
